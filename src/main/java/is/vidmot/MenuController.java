8bf511060fb82f91e5c0f03c052c410604a5b693
arnaldur2112 <142839095+arnaldur2112@users.noreply.github.com>
2024-04-15 22:47:17 +0000

    Update MenuController.java

--- src/main/java/is/vidmot/MenuController.java
+++ src/main/java/is/vidmot/MenuController.java
@@ -9,52 +9,75 @@ package is.vidmot;
  *  Getur hafið nýjan leik, sett erfiðleikastig og spurt um forritið
  *
  *****************************************************************************/
+
+import is.vinnsla.Geyma;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.RadioMenuItem;
 
-import java.util.Optional;
 
 public class MenuController {
     // fastar
-    public static final String VILTU_HAETTA = "Viltu hætta? ";
-    public static final String UMFORRIT = "Gullgrafari grefur gull og fær stig ";
+    public static final String UMFORRIT = "Örvatakkar til að hreyfa sig. Leikurinn snýst um að safna eins miklu gulli og hægt er áður en tíminn lýkur. Ekki stíga í gildrurnar því þá missir þú stig.";
+    public static final String VILTU_HAETTA = "Viltu hætta?";
 
-    private GoldController goldController;  // tenging í aðalcontroller
+    private GoldController goldController = new GoldController();  // tenging í aðalcontroller
+
+    private GoldControllerTveir goldControllerTveir = new GoldControllerTveir();
+
+    public int leikmenn = 1;
+
+    public int erfidleikastig = 1;
+
 
     /**
      * Setur erfiðleikastig - létt, miðlungs erfitt
+     *
      * @param actionEvent
      */
     public void onErfidleikastig(ActionEvent actionEvent) {
         goldController.setErfidleikastig
                 (Integer.parseInt(((RadioMenuItem) actionEvent.getSource()).getId()));
+        goldControllerTveir.setErfidleikastig
+                (Integer.parseInt(((RadioMenuItem) actionEvent.getSource()).getId()));
     }
 
     /**
      * Hefur nýjan leik
+     *
      * @param actionEvent
      */
     public void onNyrLeikur(ActionEvent actionEvent) {
-        goldController.hefjaLeik();
-        goldController.raesaKlukku();
-    }
 
-    /**
-     * Hættir í forriti
-     * @param actionEvent
-     */
-    public void onHaetta(ActionEvent actionEvent) {
-        Alert alert = new Alert(Alert.AlertType.WARNING, VILTU_HAETTA);
-        Optional<ButtonType> optional = alert.showAndWait();
-        if (optional.isPresent() && optional.get().equals(ButtonType.OK)) {
-            System.exit(0);
+        if (Geyma.getLeikmenn() == 2) {
+            goldControllerTveir.hefjaLeik();
+            goldControllerTveir.raesaKlukku();
+        } else {
+            goldController.hefjaLeik();
+            goldController.raesaKlukku();
         }
     }
 
     /**
+     * Hættir í forriti
+     *
+     * @param actionEvent
+     */
+    public void onHaetta(ActionEvent actionEvent) {
+
+        System.exit(0);
+
+    }
+
+    public void onTilBaka(ActionEvent actionEvent) {
+        ViewSwitcher.switchTo(View.MAINMENU, true);
+    }
+
+
+    /**
      * Veitir upplýsingar um forritið
+     *
      * @param actionEvent
      */
     public void onUmForritid(ActionEvent actionEvent) {
@@ -69,4 +92,51 @@ public class MenuController {
     }
 
 
+    public void setController(GoldControllerTveir goldController) {
+        this.goldControllerTveir = goldController;
+    }
+
+
+    /**
+     * Stillir gamemode
+     *
+     * @param actionEvent
+     */
+
+
+    public void onLeikmenn(ActionEvent actionEvent) {
+        leikmenn = Integer.parseInt(((RadioButton) actionEvent.getSource()).getId());
+        Geyma.setLeikmenn(leikmenn);
+    }
+
+    /**
+     * Stillir erfiðleikastig fyrir leik
+     *
+     * @param actionEvent
+     */
+
+
+    public void mainMenuErfidleikastig(ActionEvent actionEvent) {
+        erfidleikastig = Integer.parseInt(((RadioButton) actionEvent.getSource()).getId());
+        Geyma.setErfidleikastig(erfidleikastig);
+    }
+
+
+    /**
+     * Birtir tilsvarandi leikborð eftir völdnu gamemode
+     *
+     * @param actionEvent
+     */
+
+    public void birtaLeikbord(ActionEvent actionEvent) {
+
+        if (leikmenn == 1) {
+            ViewSwitcher.switchTo(View.LEIKUREINN, false);
+        }
+        if (leikmenn == 2) {
+            ViewSwitcher.switchTo(View.LEIKURTVEIR, false);
+        }
+
+
+    }
 }
